3cfa4cf1f235864f042ffce62ac5e1bf23937df3
rodrigo740 <rodrigomartins@ua.pt>
2021-11-13 16:59:06 +0000

    Ex8 still not working

---
+++ ex8.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include<fstream>
+#include<vector>
+#include <limits>
+#include <map>
+#include <math.h>
+#include "../AudioFile/AudioFile.h"
+
+using namespace std;
+
+int main(int argc, char **argv){
+    if(argc != 3){
+        cerr << "Usage: ./ex8 <input_sound> <quantization>\nExample: ./ex8 audio/noise.wav 8" << endl;
+        return -1;
+    }
+    AudioFile<float> audioFile;
+    AudioFile<float> output;
+    audioFile.load(argv[1]);
+    int numSamples = audioFile.getNumSamplesPerChannel();
+    int numChannels = audioFile.getNumChannels();
+    output.setNumChannels(numChannels);
+    output.setNumSamplesPerChannel(numSamples);
+    output.setBitDepth (audioFile.getBitDepth());
+    output.setSampleRate (audioFile.getSampleRate());
+    cout << "Channels: " <<  numChannels << endl;
+    cout << "Samples: " << numSamples << endl;
+
+    //Variaveis das equacoes
+    int delta = stoi(argv[2]);
+    for(int i=0; i<numChannels; i++){
+        for(int j=0; j<numSamples; j++){
+            float k = floor( (audioFile.samples[i][j] / delta) + 0.5);
+            output.samples[i][j] = k * delta;
+        }
+    }
+    output.save ("audio/ex8.wav", AudioFileFormat::Wave);    
+    return 0;
+}
